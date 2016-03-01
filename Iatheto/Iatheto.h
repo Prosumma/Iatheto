df0fb3aa6ce8f2e326fbc972628ee3004a8a269f
Gregory Higley <code@revolucent.net>
2016-03-01 12:23:52 -0500

    Initial commit

---
+++ Iatheto/Iatheto.h
@@ -0,0 +1,19 @@
+//
+//  Iatheto.h
+//  Iatheto
+//
+//  Created by Gregory Higley on 3/1/16.
+//  Copyright © 2016 Gregory Higley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Iatheto.
+FOUNDATION_EXPORT double IathetoVersionNumber;
+
+//! Project version string for Iatheto.
+FOUNDATION_EXPORT const unsigned char IathetoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Iatheto/PublicHeader.h>
+
+
