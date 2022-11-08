8dee417a9a275fd0048cd9cfa14df27d08e259fa
Gregory Higley <greg@prosumma.com>
2022-11-08 14:42:06 -0500

    Redo

---
+++ Sources/Iatheto/Iatheto.swift
@@ -0,0 +1,6 @@
+public struct Iatheto {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/IathetoTests/IathetoTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Iatheto
+
+final class IathetoTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Iatheto().text, "Hello, World!")
+    }
+}
